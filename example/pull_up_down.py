68d35f4f4199a9bc6f9a65e1baaad826bb25cb74
DikShv <dik-04@mail.ru>
2023-06-08 16:15:56 +0200

    test

--- example/pull_up_down.py
+++ example/pull_up_down.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import OPi.GPIO as GPIO
-from time import sleep          # this lets us have a time delay
 
-GPIO.setboard(GPIO.ZERO)        # Orange Pi Zero board
-GPIO.setmode(GPIO.BOARD)        # set up BOARD GPIO numbering
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_OFF)    # set pin 15 as input (button)
-GPIO.setup(11, GPIO.OUT)        # set pin 11 as an output (LED)
+from RepkaPi import GPIO
+
+from time import sleep          # позволяет выставить задержку на время
+
+GPIO.setboard(GPIO.REPKAPI3)        # Repka Pi 3
+GPIO.setmode(GPIO.BOARD)        # выбираем тип обращения к GPIO по номеру PIN (BOARD)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    # устанавливаем pin 15 в input (Кнопка)
+GPIO.setup(11, GPIO.OUT)        # устанавливаем pin 11 как output (LED)
 
 try:
-    while True:                 # this will carry on until you hit CTRL+C
-        if GPIO.input(15):      # if pin 15 == 1
-            print "Port 15 is 1/HIGH/True - LED ON"
-            GPIO.output(11, 1)  # set port/pin value to 1/HIGH/True
+    while True:                 # цикл будет выполняться пока не нажмем CTRL+C
+        if GPIO.input(15) == 1:      # if pin 15 == 1
+            print ("PIN 15 равен 1/HIGH/True - LED ON")
+            GPIO.output(11, 1)  # выставляем pin 11 1/HIGH/True
         else:
-            print "Port 15 is 0/LOW/False - LED OFF"
-            GPIO.output(11, 0)  # set port/pin value to 0/LOW/False
-        sleep(0.1)              # wait 0.1 seconds
+            print ("PIN 15 равен 0/LOW/False - LED OFF")
+            GPIO.output(11, 0)  # выставляем pin 11 0/LOW/False
+        sleep(0.1)              # задержка 0.1 секунда
 
-finally:                        # this block will run no matter how the try block exits
+finally:                        # выполняет блок инструкций в любом случае, было ли исключение, или нет
     print("Finally")
     GPIO.output(11, 0)
-    GPIO.cleanup()              # clean up after yourself
+    GPIO.cleanup()              # убираем все настойки по GPIO
